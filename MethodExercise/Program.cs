0bfc847dfbfe493e3eac0a43e9d295992938360e
Rubi Meneses <rubi.ventura@icloud.com>
2022-08-08 11:16:36 -0500

    finished profile configuration

--- MethodExercise/Program.cs
+++ MethodExercise/Program.cs
@@ -17,6 +17,14 @@ namespace MethodExercise
 
             Console.WriteLine("Great! Now what is your favorite band?");
             var band = Console.ReadLine();
+
+            Console.WriteLine($"Thanks, {userName}! Here is your profile");
+            Console.WriteLine("-----------------------------------------");
+
+            Console.WriteLine($"Name: {userName}");
+            Console.WriteLine($"Favorite color: {color}");
+            Console.WriteLine($"Favorite Animal: {animal}");
+            Console.WriteLine($"Favorite Band: {band}");
         }
     }
 }

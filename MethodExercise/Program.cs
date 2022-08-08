5af968eead2f7fa9cccdbeefcceda5c84cef49ef
Rubi Meneses <rubi.ventura@icloud.com>
2022-08-08 11:02:44 -0500

    started user questions

---
+++ MethodExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, What is your first name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hi, {userName}! What is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"{color} is a great color! What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("Great! Now what is your favorite band?");
+            var band = Console.ReadLine();
+        }
+    }
+}
+
